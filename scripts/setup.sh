788786956fe32e16ecc5fbe3d2ab01fb193364a2
Francesco Iannuzzelli <fi@dadi.co>
2018-04-30 22:45:11 +0200

    Custom Javascript

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,7 +9,7 @@ source $CURRENTDIR/config.sh
 
 cd $PCKDIR
 ln -s $PCKUIDIR/xsl xsl
-ln -s $PCKUIDIR/js pub/jsc
+ln -s $PCKUIDIR/js pub/js/custom
 ln -s $PCKUIDIR/css pub/css
 ln -s $PCKUIDIR/custom uploads/custom
 ln -s $PCKUIDIR/graphics/orig graphics/orig
