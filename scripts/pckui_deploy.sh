7e51ca6e467f779b9b80b2efe6a12b3b75342f6b
Francesco Iannuzzelli <fi@dadi.co>
2018-06-22 22:34:23 +0100

    Bust cache

--- scripts/pckui_deploy.sh
+++ scripts/pckui_deploy.sh
@@ -49,4 +49,8 @@ GIT_BRANCH=${GIT_BRANCH##refs/heads/}
 DEPLOY_TS=$(date +"%d/%m/%Y %H:%M:%S")
 VERSION_INFO="pckui $PCKUI_VERSION - $GIT_BRANCH $GIT_REF_SHORT - $DEPLOY_TS"
 
+# Update cache bust with git ref
+sed -i 's/CACHE_BUST/'$GIT_REF_SHORT'/g' $DEPLOYDIR/xsl/0/common.xsl
+sed -i 's/CACHE_BUST/'$GIT_REF_SHORT'/g' $DEPLOYDIR/xsl/0/root.xsl
+
 echo "$VERSION_INFO" | sudo tee $DEPLOYDIR/css/version.txt
